be07e0975880194c59d3cc5875dd913640b5c9a0
Krzysztof Dąbek <k.narg.dabek@gmail.com>
2017-04-27 23:23:42 +0200

    Zmiana koncepcji. Teraz ma sens

--- SkryptBazyDanych/MeteoAnalysisDatabase.sql
+++ SkryptBazyDanych/MeteoAnalysisDatabase.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS [MeteoAnalysisDatabase];
-CREATE DATABASE IF NOT EXISTS [MeteoAnalysisDatabase];
-USE [MeteoAnalysisDatabase];
-DROP TABLE IF EXISTS [MeteoAnalysisDatabase];
+DROP DATABASE IF EXISTS MeteoAnalysisDatabase;
+CREATE DATABASE IF NOT EXISTS MeteoAnalysisDatabase;
+USE MeteoAnalysisDatabase;
+DROP TABLE IF EXISTS MeteoAnalysisDatabase;
 
 create table Administratorzy
 (PRIMARY KEY(AdminID),
@@ -20,9 +20,8 @@ nazwisko TINYTEXT,
 e_mail TINYTEXT);
 
 create table StacjePogodowe
-(PRIMARY KEY(StationsID),
-NazwaStacjiPogodowej VARCHAR(15), #FK
-PolozenieStacji INT, #FK
+(PRIMARY KEY(StationID),
+NazwaStacjiPogodowej VARCHAR(15),
 AutomatycznyKomentarz TEXT,
 KomentarzAnalityka TEXT);
 
@@ -30,33 +29,41 @@ create table Polozenie
 (PRIMARY KEY(LocationID),
 SzerokoscGeograficzna VARCHAR(8),
 DlugoscGeograficzna VARCHAR(8),
-Wysokosc VARCHAR(4));
+Wysokosc VARCHAR(4),
+foreign key (StationID) references StacjePogodowe(StationID));
 
-create table StacjaPogodowa
-(PRIMARY KEY(MeteoStationID),
+create table PomiaryPodstawowe
+(PRIMARY KEY(MeasurementID),
 DataPomiaru DATE,
 GodzinaPomiaru TIME,
-TemperaturaSrednia FLOAT(3,1),
 Wilgotnosc FLOAT(3,1),
-WiatrPredkosc FLOAT(3,1),
 Cisnienie FLOAT(5,1),
 Opady FLOAT(3,1),
-ZachmurzenieCalkowite FLOAT(2,1),
-Widocznosc FLOAT(3,1));
+Widocznosc FLOAT(3,1),
+foreign key (TemperaturaSrednia) references Temperatura(srednia),
+foreign key (WiatrPredkosc) references Wiatr(Predkosc),
+foreign key (ZachmurzenieCalkowite) references Zachmurzenie(Calkowite),
+foreign key (StationID) references StacjePogodowe(StationID));
 
 create table Zachmurzenie
-(PRIMARY KEY(CloudsID),
+(PRIMARY KEY(CloudID),
 Calkowite FLOAT(2,1),
-Dolne FLOAT(2,1));
+Dolne FLOAT(2,1),
+foreign key (MeaserementID) references PomiaryPodstawowe(MeasurementID),
+foreign key (StationID) references StacjePogodowe(StationID));
 
 create table Wiatr
 (PRIMARY KEY(WindID),
 Kierunek VARCHAR(4),
 Predkosc FLOAT(3,1),
-Porywy FLOAT(3,1));
+Porywy FLOAT(3,1),
+foreign key (MeaserementID) references PomiaryPodstawowe(MeasurementID),
+foreign key (StationID) references StacjePogodowe(StationID));
 
 create table Tempteratura
 (PRIMARY KEY(TemperatureID),
 minimalna FLOAT(3,1),
 srednia FLOAT(3,1),
-maksymalna FLOAT(3,1));
+maksymalna FLOAT(3,1),
+foreign key (MeaserementID) references PomiaryPodstawowe(MeasurementID),
+foreign key (StationID) references StacjePogodowe(StationID));
