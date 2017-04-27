8d3b1141ea6268865e6f323fc479d061f9df1a23
Krzysztof Dąbek <k.narg.dabek@gmail.com>
2017-04-27 22:25:47 +0200

    Dodano wszystkie tabele

--- SkryptBazyDanych/MeteoAnalysisDatabase.sql
+++ SkryptBazyDanych/MeteoAnalysisDatabase.sql
@@ -3,20 +3,6 @@ CREATE DATABASE IF NOT EXISTS [MeteoAnalysisDatabase];
 USE [MeteoAnalysisDatabase];
 DROP TABLE IF EXISTS [MeteoAnalysisDatabase];
 
-create table StacjePogodowe
-(StationsID INT,
-AutomatycznyKomentarz TEXT,
-KomentarzAnalityka TEXT);
-
-create table StacjaPogodowa
-(MeteoStationID VARCHAR(15),
-DataPomiaru DATE(),
-GodzinaPomiaru TIME(),
-Wilgotnosc FLOAT(3,1),
-Cisnienie FLOAT(5,1),
-Opady FLOAT(3,1),
-Widocznosc FLOAT(3,1));
-
 create table Administratorzy
 (AdminID INT,
 login TINYTEXT,
@@ -33,4 +19,44 @@ imie TINYTEXT,
 nazwisko TINYTEXT,
 e_mail TINYTEXT);
 
-create table
+create table StacjePogodowe
+(StationsID INT,
+NazwaStacjiPogodowej VARCHAR(15), #FK
+PolozenieStacji INT, #FK
+AutomatycznyKomentarz TEXT,
+KomentarzAnalityka TEXT);
+
+create table Polozenie
+(LocationID INT,
+SzerokoscGeograficzna VARCHAR(8),
+DlugoscGeograficzna VARCHAR(8),
+Wysokosc VARCHAR(4));
+
+create table StacjaPogodowa
+(MeteoStationID VARCHAR(15),
+DataPomiaru DATE,
+GodzinaPomiaru TIME,
+TemperaturaSrednia FLOAT(3,1),
+Wilgotnosc FLOAT(3,1),
+WiatrPredkosc FLOAT(3,1),
+Cisnienie FLOAT(5,1),
+Opady FLOAT(3,1),
+ZachmurzenieCalkowite FLOAT(2,1),
+Widocznosc FLOAT(3,1));
+
+create table Zachmurzenie
+(CloudsID INT,
+Calkowite FLOAT(2,1),
+Dolne FLOAT(2,1));
+
+create table Wiatr
+(WindID INT,
+Kierunek VARCHAR(4),
+Predkosc FLOAT(3,1),
+Porywy FLOAT(3,1));
+
+create table Tempteratura
+(TemperatureID INT,
+minimalna FLOAT(3,1),
+srednia FLOAT(3,1),
+maksymalna FLOAT(3,1));
