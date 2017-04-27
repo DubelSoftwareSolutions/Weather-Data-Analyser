d3da3820857c0bc2581844f32e4dcbd734ee0028
Krzysztof Dąbek <k.narg.dabek@gmail.com>
2017-04-27 22:03:13 +0200

    Stworzono kilka tabel

--- SkryptBazyDanych/MeteoAnalysisDatabase.sql
+++ SkryptBazyDanych/MeteoAnalysisDatabase.sql
@@ -4,5 +4,33 @@ USE [MeteoAnalysisDatabase];
 DROP TABLE IF EXISTS [MeteoAnalysisDatabase];
 
 create table StacjePogodowe
-(StationsID 
+(StationsID INT,
+AutomatycznyKomentarz TEXT,
+KomentarzAnalityka TEXT);
 
+create table StacjaPogodowa
+(MeteoStationID VARCHAR(15),
+DataPomiaru DATE(),
+GodzinaPomiaru TIME(),
+Wilgotnosc FLOAT(3,1),
+Cisnienie FLOAT(5,1),
+Opady FLOAT(3,1),
+Widocznosc FLOAT(3,1));
+
+create table Administratorzy
+(AdminID INT,
+login TINYTEXT,
+haslo TINYTEXT,
+imie TINYTEXT,
+nazwisko TINYTEXT,
+e_mail TINYTEXT);
+
+create table Analitycy
+(AnalystID INT,
+login TINYTEXT,
+haslo TINYTEXT,
+imie TINYTEXT,
+nazwisko TINYTEXT,
+e_mail TINYTEXT);
+
+create table
