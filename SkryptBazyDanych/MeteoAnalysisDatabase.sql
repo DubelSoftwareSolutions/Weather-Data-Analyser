300f4c8d2b3278d815c0495927fa8cad68cf94d8
Krzysztof Dąbek <k.narg.dabek@gmail.com>
2017-04-27 21:49:43 +0200

    Dodano plik SQL z tworzeniem databazy

---
+++ SkryptBazyDanych/MeteoAnalysisDatabase.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS [MeteoAnalysisDatabase];
+CREATE DATABASE IF NOT EXISTS [MeteoAnalysisDatabase];
+USE [MeteoAnalysisDatabase];
+DROP TABLE IF EXISTS [MeteoAnalysisDatabase];
+
+create table StacjePogodowe
+(StationsID 
+
