e3fefb4377e80489c39a9373733991f4fcfa8af5
Krzysztof Dąbek <k.narg.dabek@gmail.com>
2017-05-04 15:31:57 +0200

    Poprawiono raport kwietniowy

--- SkryptBazyDanych/MeteoAnalysisDatabase.sql
+++ SkryptBazyDanych/MeteoAnalysisDatabase.sql
@@ -39,47 +39,12 @@ Wysokosc VARCHAR(4),
 StationID INT,
 foreign key (StationID) references StacjePogodowe(StationID) ON UPDATE CASCADE ON DELETE RESTRICT);
 
-create table PomiaryPodstawowe
+create table Pomiary
 (PRIMARY KEY(MeasurementID),
 MeasurementID INT,
 DataPomiaru DATE,
 GodzinaPomiaru TIME,
-Wilgotnosc FLOAT(3,1),
-Cisnienie FLOAT(5,1),
-Opady FLOAT(3,1),
-Widocznosc FLOAT(3,1),
+KodPomiaru VARCHAR(5),
+WartoscPomiaru FLOAT(5,1),
 StationID INT,
 foreign key (StationID) references StacjePogodowe(StationID) ON UPDATE CASCADE ON DELETE RESTRICT);
-
-create table Zachmurzenie
-(PRIMARY KEY(CloudID),
-CloudID INT,
-Calkowite FLOAT(2,1),
-Dolne FLOAT(2,1),
-MeaserementID INT,
-StationID INT,
-foreign key (MeaserementID) references PomiaryPodstawowe(MeasurementID) ON UPDATE CASCADE ON DELETE RESTRICT,
-foreign key (StationID) references StacjePogodowe(StationID) ON UPDATE CASCADE ON DELETE RESTRICT);
-
-create table Wiatr
-(PRIMARY KEY(WindID),
-WindID INT,
-Kierunek VARCHAR(4),
-Predkosc FLOAT(3,1),
-Porywy FLOAT(3,1),
-MeaserementID INT,
-StationID INT,
-foreign key (MeaserementID) references PomiaryPodstawowe(MeasurementID) ON UPDATE CASCADE ON DELETE RESTRICT,
-foreign key (StationID) references StacjePogodowe(StationID) ON UPDATE CASCADE ON DELETE RESTRICT);
-
-create table Tempteratura
-(PRIMARY KEY(TemperatureID),
-TemperatureID INT,
-minimalna FLOAT(3,1),
-srednia FLOAT(3,1),
-maksymalna FLOAT(3,1),
-MeaserementID INT,
-StationID INT,
-foreign key (MeaserementID) references PomiaryPodstawowe(MeasurementID) ON UPDATE CASCADE ON DELETE RESTRICT,
-foreign key (StationID) references StacjePogodowe(StationID) ON UPDATE CASCADE ON DELETE RESTRICT);
-
